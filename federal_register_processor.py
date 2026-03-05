1076ab32bcdf6bf0097553ce0fb4b3a60228b9b5
dje09 <dorothyjoy.espinosa@thomsonreuters.com>
2026-03-06 00:22:27 +0800

    Update federal_register_processor.py

--- federal_register_processor.py
+++ federal_register_processor.py
@@ -1275,4 +1275,6 @@ if __name__ == '__main__':
     print("Open your browser and navigate to: http://localhost:5000")
     print("="*60 + "\n")
     
-    app.run(debug=True, host='0.0.0.0', port=5000)
+
+    import os
+app.run(debug=False, host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
